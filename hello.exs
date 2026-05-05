044b3571cc790cecaa020c4aa219611d80efc2cb
jibin jose <jibinjose884@gmail.com>
2026-05-05 21:58:52 +0530

    Add metadata comments to hello.exs
    Added metadata comments including file name, description, and author.

--- hello.exs
+++ hello.exs
@@ -1,1 +1,7 @@
+# -----------------------------------------
+# File Name: hello.exs
+# Description: Basic Elixir program to print a message
+# Author: Jibin Jose
+# -----------------------------------------
+
 IO.puts("Hello, Elixir!")
