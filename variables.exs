844cea076c004e3d42bb5700bc0a1a58ebed003a
jibin jose <jibinjose884@gmail.com>
2026-05-06 06:46:22 +0530

    Add Elixir program for variable demonstration
    Demonstrate variables and string interpolation in Elixir.

---
+++ variables.exs
@@ -0,0 +1,19 @@
+# -----------------------------------------
+# File Name: variables.exs
+# Description: Elixir program demonstrating variables and string interpolation
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# elixir variables.exs
+
+# Output:
+# Name: Jibin
+# Age: 22
+# -----------------------------------------
+
+name = "Jibin"
+age = 22
+
+IO.puts("Name: #{name}")
+IO.puts("Age: #{age}")
