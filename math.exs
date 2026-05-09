fe83af716c7cdd1d9f6111ecbde46bff3451b948
jibin jose <jibinjose884@gmail.com>
2026-05-09 09:03:47 +0530

    Add basic arithmetic operations in math.exs
    This Elixir program demonstrates basic arithmetic operations including addition, subtraction, multiplication, and division.

---
+++ math.exs
@@ -0,0 +1,23 @@
+# -----------------------------------------
+# File Name: math.exs
+# Description: Elixir program demonstrating basic arithmetic operations
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# elixir math.exs
+
+# Output:
+# Add: 15
+# Sub: 5
+# Mul: 50
+# Div: 2.0
+# -----------------------------------------
+
+a = 10
+b = 5
+
+IO.puts("Add: #{a + b}")
+IO.puts("Sub: #{a - b}")
+IO.puts("Mul: #{a * b}")
+IO.puts("Div: #{a / b}")
