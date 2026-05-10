f8d0f3b956e9a4b4e21b4e8972e5b16066831040
jibin jose <jibinjose884@gmail.com>
2026-05-10 13:46:08 +0530

    Create if_else.exs for age checking
    Add Elixir program demonstrating if-else condition.

---
+++ if_else.exs
@@ -0,0 +1,20 @@
+# -----------------------------------------
+# File Name: if_else.exs
+# Description: Elixir program demonstrating if-else condition
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# elixir if_else.exs
+
+# Output:
+# Adult
+# -----------------------------------------
+
+age = 18
+
+if age >= 18 do
+  IO.puts("Adult")
+else
+  IO.puts("Minor")
+end
