c66a0d2f10a52589ba9e55cea45f706baf124a9b
jibin jose <jibinjose884@gmail.com>
2026-05-12 06:50:23 +0530

    Create pattern.exs for pattern matching example
    Add Elixir program demonstrating pattern matching.

---
+++ pattern.exs
@@ -0,0 +1,16 @@
+# -----------------------------------------
+# File Name: pattern.exs
+# Description: Elixir program demonstrating pattern matching
+# Author: Jibin Jose
+# -----------------------------------------
+
+# Running Command:
+# elixir pattern.exs
+
+# Output:
+# a=10, b=20
+# -----------------------------------------
+
+{a, b} = {10, 20}
+
+IO.puts("a=#{a}, b=#{b}")
